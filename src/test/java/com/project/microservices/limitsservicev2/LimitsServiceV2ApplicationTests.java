c5682e93b7bb6ebc914c345b9b313420e33e3ca4
Himanshu Rajvanshi <himanshu.rajvanshi@synoriq.in>
2021-10-12 14:56:01 +0530

    initial commit

---
+++ src/test/java/com/project/microservices/limitsservicev2/LimitsServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservices.limitsservicev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
